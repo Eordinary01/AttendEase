10139826a2eb266d89e5d9b131065070861bac35
Parth <parth.manocha2901@gmail.com>
2024-05-22 19:35:51 +0530

    changes..fff

--- frontend/src/component/header/index.jsx
+++ frontend/src/component/header/index.jsx
@@ -5,28 +5,20 @@ const Header = ({ isAuthenticated, onLogout, isRegistered }) => {
   return (
     <div className="bg-purple-900 p-4 flex justify-between items-center">
       <div>
-        <h1 className="text-white text-xl font-bold">Attend Ease</h1> 
+        <h1 className="text-white text-xl font-bold">Attend Ease</h1>
       </div>
       <div>
-        {!isAuthenticated &&(
+        {isAuthenticated && (
           <button onClick={onLogout} className="text-white mx-2">
             Logout
           </button>
-
-          
-
         )}
-        {!isAuthenticated && !isRegistered && (
-          <Link to="/login" className="text-white mx-2"> 
-            Login
-          </Link>
-        )}
-        {!isAuthenticated && !isRegistered && (
+        {!isAuthenticated && (
           <>
-            <Link to="/register" className="text-white mx-2"> 
+            <Link to="/register" className="text-white mx-2">
               Register
             </Link>
-            <Link to="/login" className="text-white mx-2"> 
+            <Link to="/login" className="text-white mx-2">
               Login
             </Link>
           </>
@@ -34,7 +26,7 @@ const Header = ({ isAuthenticated, onLogout, isRegistered }) => {
         <Link to="/tickets" className="text-white mx-2">
           Create Ticket
         </Link>
-        <Link to={"/dashboard"} className='text-white mx-2'>
+        <Link to="/dashboard" className="text-white mx-2">
           Dashboard
         </Link>
       </div>
