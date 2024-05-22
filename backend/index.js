144d60c9fbf4da1f75f3fb72afd7ea6c386103ad
Parth <parth.manocha2901@gmail.com>
2024-05-22 07:18:53 +0530

    changes.........

--- backend/index.js
+++ backend/index.js
@@ -20,7 +20,7 @@ const corsOptions = {
   origin: (origin, callback) => {
     // Allow requests with no origin (like mobile apps or curl requests)
     if (!origin) return callback(null, true);
-    if (['https://attend-ease-f.vercel.app/'].indexOf(origin) === -1) {
+    if (['https://attend-ease-f.vercel.app/','https://attend-ease-jll12ou7i-extraordinarys-projects.vercel.app/'].indexOf(origin) === -1) {
       return callback(new Error('Not allowed by CORS'));
     }
     return callback(null, true);
